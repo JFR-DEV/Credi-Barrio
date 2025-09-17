488ca3d184853b684900e31eb8d729c62ab855a1
JFR-DEV <xpfredy@gmail.com>
2025-09-17 14:47:03 -0300

    Create CollectorApp.tsx

---
+++ components/CollectorApp.tsx
@@ -0,0 +1,11 @@
+// components/CollectorApp.tsx
+import React from "react";
+
+export function CollectorApp() {
+  return (
+    <div>
+      <h1>Aplicación del Cobrador</h1>
+      <p>Esta sección está en desarrollo.</p>
+    </div>
+  );
+}
